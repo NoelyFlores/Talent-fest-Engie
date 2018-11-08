01d1c50b66cb8e05f82bbd31ce9f890d29c439df
ticze <ticze2598@gmail.com>
2018-11-08 11:45:53 -0500

    estructura de datos profecionales

--- src/components/professionalData.vue
+++ src/components/professionalData.vue
@@ -1,61 +1,48 @@
 <template>
+<v-container grid-list-md text-xs>
   <v-layout row justify-center>
-      <v-card>
-        <v-card-text>
-          <v-container grid-list-md>
-            <v-layout wrap>
-              <v-flex xs12 sm6>
-                <v-select
-                  :items="['Sin educación formal', 'Educación especial incompleta', 'Educación especial completa', 'Educacion primaria completa', 'Educación secundaria completa', 'Técnica completa', 'Universidad completa', 'Grado de bachiller', 'Titulado', 'Maestría incompleta', 'Mestría completa','Grado de doctor']"
-                  label="Grado de Estudio"
-                  required
-                ></v-select>
-              </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Legal first name*" required></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field
-                  label="Legal last name*"
-                  hint="example of persistent helper text"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field label="Email*" required></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field label="Password*" type="password" required></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6>
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  label="Age*"
-                  required
-                ></v-select>
-              </v-flex>
-              <v-flex xs12 sm6>
-                <v-autocomplete
-                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                  label="Interests"
-                  multiple
-                ></v-autocomplete>
-              </v-flex>
-            </v-layout>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-  </v-layout>
+    <v-flex xs12>
+      <v-flex xs12>
+      <h3>Estudios superiores:</h3>
+      </v-flex>
+      <v-layout row  justify-center>
+      <v-flex xs12 sm6> 
+      <v-btn outline color="indigo">Maestria completa</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6>
+      <v-btn outline color="indigo">Agregar</v-btn>
+      </v-flex>
+      </v-layout>
+      <v-flex xs12 sm6>
+        <h3>Datos de estudio:</h3>
+        <v-select v-model="gradoProfesion"  
+          :items="['Sin educación formal', 'Educación especial incompleta', 'Educación especial completa', 'Educacion primaria completa', 'Educación secundaria completa', 'Técnica completa', 'Universidad completa', 'Grado de bachiller', 'Titulado', 'Maestría incompleta', 'Maestría completa','Grado de doctor']"
+          label="Grado de Estudio"
+          required
+        ></v-select>
+        <h3>Tipo de institución</h3>
+        <v-select v-model="tipoInstituto"
+          :items="['Educaión superior de formacion artística','Instituto superior pedagógico','Instituto de educacion superior tecnológica','Universidad']"
+          label="Tipo de institución"
+          required>
+        </v-select>
+        <h3>Descripción de carrera</h3>
+        <v-text-field  v-model="profesión" label="Profesión" required></v-text-field>
+        <v-text-field v-model="institucion" label="Nombre de institución" required></v-text-field>
+        <h4>Desde:</h4> 
+        <v-flex xs6>
+        <v-text-field  v-model="desde" required></v-text-field>
+        </v-flex>
+        <h4>Hasta:</h4> 
+        <v-flex xs6>
+        <v-text-field  v-model="hasta" required></v-text-field>
+        </v-flex>
+        <v-text-field  v-model="duracion" label="Duración" required></v-text-field>
+      <v-text-field v-model="lugar" label="Lugar de institución" required></v-text-field>
+      </v-flex>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
 import firebase from 'firebase'
@@ -63,14 +50,39 @@ export default {
     name:'profData',
     data(){
         return {
-
+          gradoProfesion :"",
+          tipoInstituto:"",
+          profesión:"",
+          institucion:"",
+          desde:"",
+          hasta:"",
+          duracion:"",
+          lugar:""
         }
     }, 
 	mounted(){},
-	created(){},
+	created(){
+    this.firebaseDataProfesion();
+  },
 	watch: {},
 	computed:{},
-	methods:{},
+	methods:{
+    firebaseDataProfesion(){
+      let dataEstudios = firebase.database().ref().child("usuario/0001ED/datos_profesionales");
+      dataEstudios.on("value",dataP =>{
+        const data = dataP.val()
+        console.log(data)
+        this.gradoProfesion = data.datos_de_estudios
+        this.tipoInstituto = data.descripción_tipo_de_institución
+        this.profesión = data.descripción_de_carrera.profesión
+        this.institucion = data.descripción_de_carrera.nombre_de_institucion        
+        this.desde = data.descripción_de_carrera.desde
+        this.hasta = data.descripción_de_carrera.hasta
+        this.duracion = data.descripción_de_carrera.duracion
+        this.lugar = data.descripción_de_carrera.lugar
+      })
+    }
+  },
 	components:{}
   }
 </script>
